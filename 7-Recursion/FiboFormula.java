a309417c64a1b50387c32ef3e13d6e2d7c8aff92
Preet Sojitra <preet.dev373@gmail.com>
2023-06-14 00:19:20 +0530

    add(7-Recursion): solved fibonacci using formula derived from solving the recurrence relation

---
+++ 7-Recursion/FiboFormula.java
@@ -0,0 +1,11 @@
+public class FiboFormula {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(fiboFormula(i));
+        }
+    }
+
+    static int fiboFormula(int n) {
+        return (int) ((Math.pow(((1 + Math.sqrt(5)) / 2), n) - Math.pow(((1 - Math.sqrt(5)) / 2), n)) / Math.sqrt(5));
+    }
+}
